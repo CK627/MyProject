8b435c2da4a8195ff57d639787ec977045863936
CK627 <3340324320@qq.com>
2026-04-11 19:30:57 +0800

    chore: remove SmartCampusServicePlatform from main branch (should only exist in its own branch)

--- SmartCampusServicePlatform/backend/app/schemas/__init__.py
+++
@@ -1,9 +0,0 @@
-# Schemas package
-from .user import *
-from .auth import *
-from .post import *
-from .task import *
-from .wallet import *
-from .announcement import *
-from .friend import *
-from .message import *
--- SmartCampusServicePlatform/backend/app/websocket/__init__.py
+++
@@ -1,5 +0,0 @@
-"""WebSocket模块"""
-from .connection_manager import manager
-from .routes import router
-
-__all__ = ['manager', 'router']
